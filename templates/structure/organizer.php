3a6fa293ffe95a4b1c07f88446cec9df41dcf73e
Korvin Szanto <korvinszanto@gmail.com>
2018-04-13 20:04:31 -0700

    6x1, new image for daniele

--- templates/structure/organizer.php
+++ templates/structure/organizer.php
@@ -1,4 +1,4 @@
-<<?= isset($twitter) ? "a href='https://twitter.com/$twitter'" : 'div' ?> class="col col-12 sm-col-6 md-col-4 lg-col-4 px2 py2 organizer">
+<<?= isset($twitter) ? "a href='https://twitter.com/$twitter'" : 'div' ?> class="col col-12 sm-col-6 md-col-4 lg-col-2 px2 py2 organizer">
     <amp-layout width="1" height="1" layout="responsive" class="circle bg-blue cloth relative shadow-lg">
         <amp-img layout="fill"
                  src="<?= isset($image) ? "/images/scenes/who/$image" : "http://www.ywcabrand.org/atf/cf/%7Bc3201f87-9e48-4580-84fa-429e77844aa1%7D/BLP0062303.JPG" ?>"
